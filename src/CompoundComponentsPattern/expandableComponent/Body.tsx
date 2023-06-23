e6492f67c4ca3dd010878d2e697de1180ee096dc
songhee24 <akaratel61@gmail.com>
2023-06-23 13:39:30 +0600

    body reusable

--- src/CompoundComponentsPattern/expandableComponent/Body.tsx
+++ src/CompoundComponentsPattern/expandableComponent/Body.tsx
@@ -2,9 +2,15 @@ import { ReactNode, useContext } from "react";
 import { ExpandableContext } from "./Expandable";
 interface Props {
   children: ReactNode;
+  className: string;
 }
-const Body = ({ children }: Props) => {
+const Body = ({ children, className = "", ...otherProps }: Props) => {
   const { expanded } = useContext(ExpandableContext);
-  return <>{expanded ? children : null}</>;
+  const combinedClassNames = ["Expandable-panel", className].join("");
+  return (
+    <div {...otherProps} className={combinedClassNames}>
+      {expanded ? children : null}
+    </div>
+  );
 };
 export default Body;
