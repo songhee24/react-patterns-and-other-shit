3e835c223bf3faf08d700996ac81bbe992b8819b
songhee24 <akaratel61@gmail.com>
2023-06-26 01:37:51 +0600

    converting to useReducer pattern

--- src/CompoundComponentsPattern/hooks/useExpanded.ts
+++ src/CompoundComponentsPattern/hooks/useExpanded.ts
@@ -1,14 +1,38 @@
-import { useCallback, useMemo, useRef, useState } from "react";
+import { useCallback, useMemo, useReducer, useRef, useState } from "react";
 const callFunctionsInSequence =
   (...fns: any[]) =>
   (...args: any) =>
     fns.forEach((fn) => fn && fn(...args));
+
+const internalReducer = (
+  state: { expanded: boolean },
+  action: { type: string; payload: any }
+) => {
+  switch (action.type) {
+    case useExpanded.types.toggleExpand:
+      return {
+        ...state,
+        expanded: !state.expanded, //toggle expand state property
+      };
+    case useExpanded.types.reset:
+      return {
+        ...state,
+        expanded: action.payload, // reset expanded with a payload
+      };
+    default:
+      throw new Error(`Action type ${action.type} not handled`);
+  }
+};
 export default function useExpanded(initialExpanded = false) {
-  const [expanded, setExpanded] = useState<boolean>(initialExpanded);
+  const initialState = { expanded: initialExpanded };
+  // const [expanded, setExpanded] = useState<boolean>(initialExpanded);
   const toggle = useCallback(
     () => setExpanded((prevExpanded) => !prevExpanded),
     []
   );
+
+  const [{ expanded }, setExpanded] = useReducer(internalReducer, initialState);
+
   const resetRef = useRef(0);
 
   const reset = useCallback(() => {
@@ -37,3 +61,8 @@ export default function useExpanded(initialExpanded = false) {
     [expanded, toggle, getTogglerProps, reset]
   );
 }
+
+useExpanded.types = {
+  toggleExpand: "EXPAND",
+  reset: "RESET",
+};
