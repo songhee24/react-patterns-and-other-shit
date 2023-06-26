334f49470f7b95f634d8940110ab71e86f674a6e
songhee24 <akaratel61@gmail.com>
2023-06-26 22:50:48 +0600

    created new hook to make my expanded logic custom #2

---
+++ src/CompoundComponentsPattern/hooks/useEffectAfterMount.ts
@@ -0,0 +1,14 @@
+import { useEffect, useRef } from "react";
+
+export default function useEffectAfterMount(
+  cb: (params?: any) => void,
+  deps: any[]
+) {
+  const componentJustMounted = useRef(true);
+  useEffect(() => {
+    if (!componentJustMounted.current) {
+      return cb();
+    }
+    componentJustMounted.current = false;
+  }, deps);
+}
