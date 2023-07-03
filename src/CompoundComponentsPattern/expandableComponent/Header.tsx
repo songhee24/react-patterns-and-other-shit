3023b4c795eba81f0ea4d33b3c6dde77c2704690
songhee24 <akaratel61@gmail.com>
2023-07-03 13:35:22 +0600

    header other props type

--- src/CompoundComponentsPattern/expandableComponent/Header.tsx
+++ src/CompoundComponentsPattern/expandableComponent/Header.tsx
@@ -2,19 +2,19 @@ import React, { ReactNode, useContext } from "react";
 import { ExpandableContext } from "./Expandable";
 import "./Header.css";
 
-interface Props {
+interface Props extends React.ComponentPropsWithoutRef<"button"> {
   children: ReactNode;
   className?: string;
 }
 
-const Header = ({ children, className = "" }: Props) => {
+const Header = ({ children, className = "", ...otherProps }: Props) => {
   const { toggle } = useContext(ExpandableContext);
 
   const combinedClassName = ["Expandable-trigger", className]
     .filter(Boolean)
     .join("");
   return (
-    <button className={combinedClassName} onClick={toggle}>
+    <button className={combinedClassName} onClick={toggle} {...otherProps}>
       {children}
     </button>
   );
