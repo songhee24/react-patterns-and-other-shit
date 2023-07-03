bb1b8db76cd9b669f711b76fdbdeb1389f29c767
songhee24 <akaratel61@gmail.com>
2023-07-03 13:52:06 +0600

    Custom Use of the Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import Expandable from "./CompoundComponentsPattern/expandableComponent/Expandable";
+import "./App.css";
 
 function App() {
   return (
-    <Expandable onExpand={() => {}}>
-      <Expandable.Header style={{ color: "red" }}>
-        React hooks
-      </Expandable.Header>
-      <Expandable.Icon />
-      <Expandable.Body>Hooks are awesome</Expandable.Body>
-    </Expandable>
+    <div className="App">
+      <Expandable onExpand={() => {}}>
+        <Expandable.Header style={{ color: "red", border: "1px solid teal" }}>
+          React hooks
+        </Expandable.Header>
+        <Expandable.Icon />
+        <Expandable.Body>
+          <img
+            src="/api/collection/10370001/4597752283529216/page/5195905143668736/image/4691607934730240"
+            style={{ width: "250px" }}
+            alt="reintroducing react book cover"
+          />
+          <p style={{ opacity: 0.7 }}>
+            This book is so amazing! <br />
+            <a
+              href="https://leanpub.com/reintroducing-react"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Go get it now.
+            </a>
+          </p>
+        </Expandable.Body>
+      </Expandable>
+    </div>
   );
 }
 
