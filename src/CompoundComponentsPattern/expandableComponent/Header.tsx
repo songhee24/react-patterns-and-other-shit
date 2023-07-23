63a8d52d54e0a304b0ca60c55b69a9020cf51217
songhee24 <akaratel61@gmail.com>
2023-07-23 13:30:17 +0600

    Styling

--- src/CompoundComponentsPattern/expandableComponent/Header.tsx
+++ src/CompoundComponentsPattern/expandableComponent/Header.tsx
@@ -1,12 +1,22 @@
 import React, { ReactNode, useContext } from "react";
 import { ExpandableContext } from "./Expandable";
+import "./Header.css";
 
 interface Props {
   children: ReactNode;
+  className?: string;
 }
 
-const Header = ({ children }: Props) => {
+const Header = ({ children, className = "" }: Props) => {
   const { toggle } = useContext(ExpandableContext);
-  return <div onClick={toggle}>{children}</div>;
+
+  const combinedClassName = ["Expandable-trigger", className]
+    .filter(Boolean)
+    .join("");
+  return (
+    <button className={combinedClassName} onClick={toggle}>
+      {children}
+    </button>
+  );
 };
 export default Header;
