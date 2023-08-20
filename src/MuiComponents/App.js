a311ee1a6f4333577dbbc1d8d970608a65b78681
songhee24 <akaratel61@gmail.com>
2023-08-21 03:08:17 +0600

    button test

--- src/MuiComponents/App.js
+++ src/MuiComponents/App.js
@@ -3,7 +3,7 @@ import { Button } from "@mui/material";
 export default function App() {
   return (
     <div>
-      <Button variant="contained">Hello world</Button>
+      <Button variant="text">Hello world</Button>
     </div>
   );
 }
